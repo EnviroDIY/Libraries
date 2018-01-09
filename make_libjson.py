e58eb29e73ba98c6fd51d0671c96a7d6342ea354
SRGDamia1 <sdamiano@stroudcenter.org>
2018-01-09 16:03:53 -0500

    Updated libjson maker

--- make_libjson.py
+++ make_libjson.py
@@ -41,69 +41,89 @@ with open('.gitmodules','r') as submods:
                 dep['note'] = 'TI ADS1X15 ADC Library by Adafruit'
                 dep['library id'] = '342'
                 dep['url'] = dep.pop('version')
+                dep['authors'] = ["Adafruit"]
             if dep['name'] == 'Adafruit_DHT':
                 dep['name'] = 'DHT sensor library'
                 dep['note'] = 'AOSong DHT Sensor Library by Adafruit'
                 dep['library id'] = '19'
                 dep['url'] = dep.pop('version')
+                dep['authors'] = ["Adafruit"]
             if dep['name'] == 'Adafruit_Sensor':
                 dep['name'] = 'Adafruit Unified Sensor'
                 dep['note'] = 'Adafruit Unified Sensor Library'
                 dep['library id'] = '31'
                 dep['url'] = dep.pop('version')
+                dep['authors'] = ["Adafruit"]
             if dep['name'] == 'Adafruit_TSL2561':
                 dep['name'] = 'Adafruit TSL2561'
                 dep['note'] = 'TAOS TSL2561 Luminosity Sensor Library by Adafruit'
                 dep['library id'] = '34'
                 dep['url'] = dep.pop('version')
+                dep['authors'] = ["Adafruit"]
             if dep['name'] == 'Adafruit_AM2315':
                 dep['name'] = 'Adafruit AM2315'
                 dep['note'] = 'AOSong AM2315 I2C Temp/Humidity Sensor Library by Adafruit'
                 dep['library id'] = '773'
                 dep['url'] = dep.pop('version')
+                dep['authors'] = ["Adafruit"]
             if dep['name'] == 'Adafruit_BME280':
                 dep['name'] = 'Adafruit BME280 Library'
                 dep['note'] = 'Bosch BME280 Temp/Humidity/Pressure Sensor Library by Adafruit'
                 dep['library id'] = '166'
                 dep['url'] = dep.pop('version')
+                dep['authors'] = ["Adafruit"]
             if dep['name'] == 'EnviroDIY_SDI12':
                 dep['name'] = 'Arduino-SDI-12'
                 dep['note'] = 'EnviroDIY SDI-12 Library'
                 dep['library id'] = '1486'
                 dep['url'] = dep.pop('version')
+                dep['authors'] = ["Kevin M. Smith", "Sara Damiano", "Shannon Hicks"]
             if dep['name'] == 'Sodaq_PcInt':
                 dep['note'] = 'Sodaq Pin Change Interrupt Library'
                 dep['library id'] = '695'
+                dep['authors'] = ["Kees Bakker"]
                 dep['url'] = dep.pop('version')
             if dep['name'] == 'Sodaq_RTCTimer':
                 dep['name'] = 'RTCTimer'
                 dep['note'] = 'Sodaq Real Time Clock (RTC) Timer Library '
                 dep['library id'] = '645'
                 dep['url'] = dep.pop('version')
+                dep['authors'] = ["Kees Bakker"]
             if dep['name'] == 'SdFat':
                 dep['note'] = 'SdFat'
                 dep['library id'] = '322'
                 dep['url'] = dep.pop('version')
+                dep['authors'] = ["Bill Greiman"]
             if dep['name'] == 'OneWire':
                 dep['note'] = 'OneWire'
                 dep['library id'] = '1'
+                dep['authors'] = ["Paul Stoffregen", "Tom Pollard", "Derek Yerger", "Jim Studt", "Jason Dangel", "Robin James", "Ken Butcher", "Guillermo Lovato", "Scott Roberts", "Bertrik Sikken", "Mark Tillotson", "Glenn Trewitt", "Josh Larios"]
                 dep['url'] = dep.pop('version')
             if dep['name'] == 'DallasTemperature':
                 dep['note'] = 'DallasTemperature'
                 dep['library id'] = '54'
                 dep['url'] = dep.pop('version')
+                dep['authors'] = ["Guil Barros", "Miles Burton", "Rob Tillart", "Tim Nuewsome"]
             if dep['name'] == 'RTCZero':
                 dep['note'] = 'RTCZero'
                 dep['library id'] = '873'
                 dep['url'] = dep.pop('version')
+                dep['authors'] = ["Arduino"]
             if dep['name'] == 'SensorModbusMaster':
                 dep['note'] = 'EnviroDIY SensorModbusMaster'
                 dep['library id'] = '1824'
                 dep['url'] = dep.pop('version')
+                dep['authors'] = ["Sara Damiano"]
             if dep['name'] == 'EnableInterrupt':
                 dep['note'] = 'GreyGnome\'s EnableInterrupt'
                 dep['library id'] = '311'
                 dep['url'] = dep.pop('version')
+                dep['authors'] = ["Mike 'GreyGnome' Schwager"]
+            if dep['name'] == 'EnviroDIY_ModularSensors':
+                dep['note'] = 'EnviroDIY ModularSensors'
+                dep['library id'] = '1648'
+                dep['url'] = dep.pop('version')
+                dep['authors'] = ["Sara Damiano", "Shannon Hicks"]
 
         if (line.split()[0] == "[submodule") and num != 1:
             dependencies.append(dep)
