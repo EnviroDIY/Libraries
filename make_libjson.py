48ede50fa6c7948bed87463ea86eec3474e7c845
SRGDamia1 <sdamiano@stroudcenter.org>
2018-01-12 11:54:48 -0500

    moreupdates to json maker

--- make_libjson.py
+++ make_libjson.py
@@ -42,6 +42,18 @@ with open('.gitmodules','r') as submods:
                 dep['library id'] = '342'
                 dep['url'] = dep.pop('version')
                 dep['authors'] = ["Adafruit"]
+            if dep['name'] == 'Adafruit_AM2315':
+                dep['name'] = 'Adafruit AM2315'
+                dep['note'] = 'AOSong AM2315 I2C Temp/Humidity Sensor Library by Adafruit'
+                dep['library id'] = '773'
+                dep['url'] = dep.pop('version')
+                dep['authors'] = ["Adafruit"]
+            if dep['name'] == 'Adafruit_BME280':
+                dep['name'] = 'Adafruit BME280 Library'
+                dep['note'] = 'Bosch BME280 Temp/Humidity/Pressure Sensor Library by Adafruit'
+                dep['library id'] = '166'
+                dep['url'] = dep.pop('version')
+                dep['authors'] = ["Adafruit"]
             if dep['name'] == 'Adafruit_DHT':
                 dep['name'] = 'DHT sensor library'
                 dep['note'] = 'AOSong DHT Sensor Library by Adafruit'
@@ -60,24 +72,56 @@ with open('.gitmodules','r') as submods:
                 dep['library id'] = '34'
                 dep['url'] = dep.pop('version')
                 dep['authors'] = ["Adafruit"]
-            if dep['name'] == 'Adafruit_AM2315':
-                dep['name'] = 'Adafruit AM2315'
-                dep['note'] = 'AOSong AM2315 I2C Temp/Humidity Sensor Library by Adafruit'
-                dep['library id'] = '773'
+            if dep['name'] == 'DallasTemperature':
+                dep['note'] = 'DallasTemperature - Arduino Library for Dallas Temperature ICs (DS18B20, DS18S20, DS1822, DS1820)'
+                dep['library id'] = '54'
                 dep['url'] = dep.pop('version')
-                dep['authors'] = ["Adafruit"]
-            if dep['name'] == 'Adafruit_BME280':
-                dep['name'] = 'Adafruit BME280 Library'
-                dep['note'] = 'Bosch BME280 Temp/Humidity/Pressure Sensor Library by Adafruit'
-                dep['library id'] = '166'
+                dep['authors'] = ["Guil Barros", "Miles Burton", "Rob Tillart", "Tim Nuewsome"]
+            if dep['name'] == 'EnableInterrupt':
+                dep['note'] = 'GreyGnome\'s EnableInterrupt - Assign an interrupt to any supported pin on all Arduinos'
+                dep['library id'] = '311'
                 dep['url'] = dep.pop('version')
-                dep['authors'] = ["Adafruit"]
+                dep['authors'] = ["Mike 'GreyGnome' Schwager"]
+            if dep['name'] == 'EnviroDIY_DS3231':
+                dep['note'] = 'An Arduino library for the DS3231 RTC (Real Time Clock), based off of the Sodaq_DS3231 library.'
+                dep['library id'] = '2079'
+                dep['url'] = dep.pop('version')
+                dep['authors'] = ["Kees Bakker", "Sara Damiano"]
+            if dep['name'] == 'EnviroDIY_GPRSBee':
+                dep['name'] = 'GPRSbee'
+                dep['note'] = 'An Arduino library for the SODAQ GPRSbee.'
+                dep['authors'] = ["Kees Bakker"]
+            if dep['name'] == 'EnviroDIY_ModularSensors':
+                dep['note'] = 'EnviroDIY ModularSensors - A library that allows acces to multiple sensors through a unified interface.'
+                dep['library id'] = '1648'
+                dep['url'] = dep.pop('version')
+                dep['authors'] = ["Sara Damiano", "Shannon Hicks"]
             if dep['name'] == 'EnviroDIY_SDI12':
                 dep['name'] = 'Arduino-SDI-12'
                 dep['note'] = 'EnviroDIY SDI-12 Library'
                 dep['library id'] = '1486'
                 dep['url'] = dep.pop('version')
                 dep['authors'] = ["Kevin M. Smith", "Sara Damiano", "Shannon Hicks"]
+            if dep['name'] == 'OneWire':
+                dep['note'] = 'OneWire - Control 1-Wire protocol (DS18S20, DS18B20, DS2408 and etc)'
+                dep['library id'] = '1'
+                dep['url'] = dep.pop('version')
+                dep['authors'] = ["Paul Stoffregen", "Tom Pollard", "Derek Yerger", "Jim Studt", "Jason Dangel", "Robin James", "Ken Butcher", "Guillermo Lovato", "Scott Roberts", "Bertrik Sikken", "Mark Tillotson", "Glenn Trewitt", "Josh Larios"]
+            if dep['name'] == 'RTCZero':
+                dep['note'] = 'Functions for using the processor real time clock in SAMD21 processors'
+                dep['library id'] = '873'
+                dep['url'] = dep.pop('version')
+                dep['authors'] = ["Arduino"]
+            if dep['name'] == 'SdFat':
+                dep['note'] = 'SdFat - FAT16/FAT32 file system for SD cards.'
+                dep['library id'] = '322'
+                dep['url'] = dep.pop('version')
+                dep['authors'] = ["Bill Greiman"]
+            if dep['name'] == 'SensorModbusMaster':
+                dep['note'] = 'EnviroDIY SensorModbusMaster - Arduino library for communicating via modbus with the Arduino acting as master.'
+                dep['library id'] = '1824'
+                dep['url'] = dep.pop('version')
+                dep['authors'] = ["Sara Damiano"]
             if dep['name'] == 'Sodaq_PcInt':
                 dep['note'] = 'Sodaq Pin Change Interrupt Library'
                 dep['library id'] = '695'
@@ -89,41 +133,14 @@ with open('.gitmodules','r') as submods:
                 dep['library id'] = '645'
                 dep['url'] = dep.pop('version')
                 dep['authors'] = ["Kees Bakker"]
-            if dep['name'] == 'SdFat':
-                dep['note'] = 'SdFat'
-                dep['library id'] = '322'
-                dep['url'] = dep.pop('version')
-                dep['authors'] = ["Bill Greiman"]
-            if dep['name'] == 'OneWire':
-                dep['note'] = 'OneWire'
-                dep['library id'] = '1'
-                dep['authors'] = ["Paul Stoffregen", "Tom Pollard", "Derek Yerger", "Jim Studt", "Jason Dangel", "Robin James", "Ken Butcher", "Guillermo Lovato", "Scott Roberts", "Bertrik Sikken", "Mark Tillotson", "Glenn Trewitt", "Josh Larios"]
-                dep['url'] = dep.pop('version')
-            if dep['name'] == 'DallasTemperature':
-                dep['note'] = 'DallasTemperature'
-                dep['library id'] = '54'
-                dep['url'] = dep.pop('version')
-                dep['authors'] = ["Guil Barros", "Miles Burton", "Rob Tillart", "Tim Nuewsome"]
-            if dep['name'] == 'RTCZero':
-                dep['note'] = 'RTCZero'
-                dep['library id'] = '873'
-                dep['url'] = dep.pop('version')
-                dep['authors'] = ["Arduino"]
-            if dep['name'] == 'SensorModbusMaster':
-                dep['note'] = 'EnviroDIY SensorModbusMaster'
-                dep['library id'] = '1824'
+            if dep['name'] == 'TinyGSM':
+                dep['note'] = 'A small Arduino library for GPRS modules.'
+                dep['authors'] = ["Volodymyr Shymanskyy"]
+            if dep['name'] == 'YosemitechModbus':
+                dep['note'] = 'Arduino library for communication with Yosemitech sensors via Modbus.'
+                dep['library id'] = '2078'
                 dep['url'] = dep.pop('version')
                 dep['authors'] = ["Sara Damiano"]
-            if dep['name'] == 'EnableInterrupt':
-                dep['note'] = 'GreyGnome\'s EnableInterrupt'
-                dep['library id'] = '311'
-                dep['url'] = dep.pop('version')
-                dep['authors'] = ["Mike 'GreyGnome' Schwager"]
-            if dep['name'] == 'EnviroDIY_ModularSensors':
-                dep['note'] = 'EnviroDIY ModularSensors'
-                dep['library id'] = '1648'
-                dep['url'] = dep.pop('version')
-                dep['authors'] = ["Sara Damiano", "Shannon Hicks"]
 
         if (line.split()[0] == "[submodule") and num != 1:
             dependencies.append(dep)
@@ -135,5 +152,5 @@ submods.close()
 library['dependencies'] = dependencies
 
 jsonfile = open('library.json', 'w')
-json.dump(library, jsonfile, indent=1, separators=(',', ': '))
+json.dump(library, jsonfile, indent=4, separators=(',', ': '))
 jsonfile.close()
